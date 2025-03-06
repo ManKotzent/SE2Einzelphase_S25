48da853609ed43c0ba4234d56ed61d2ee5a7840a
Kotzent Manuel <mankotzent@edu.aau.at>
2025-03-06 14:14:52 +0100

    Adjusted LeaderboardControllerTests for time based tiebreaker

--- src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
+++ src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
@@ -3,6 +3,7 @@ package at.aau.serg.controllers
 import at.aau.serg.models.GameResult
 import at.aau.serg.services.GameResultService
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 import kotlin.test.Test
@@ -37,6 +38,29 @@ class LeaderboardControllerTests {
         assertEquals(third, res[2])
     }
 
+    @Test
+    fun test_getLeaderboard_sameScore_CorrectTimeInSecondsSorting() {
+        val first = GameResult(1, "first", 20, 10.0)
+        val second = GameResult(2, "second", 20, 15.0)
+        val third = GameResult(3, "third", 20, 20.0)
+
+        whenever(mockedService.getGameResults()).thenReturn(listOf(second, first, third))
+
+        val res: List<GameResult> = controller.getLeaderboard()
+
+        verify(mockedService).getGameResults()
+        assertEquals(3, res.size)
+        assertEquals(first, res[0])
+        assertEquals(second, res[1])
+        assertEquals(third, res[2])
+    }
+
+    /*
+    * This test is relevant to an older version of the LeaderboardController and has
+    * thus been disabled.
+    * Remove it if necessary.
+    * */
+    @Disabled
     @Test
     fun test_getLeaderboard_sameScore_CorrectIdSorting() {
         val first = GameResult(1, "first", 20, 20.0)
